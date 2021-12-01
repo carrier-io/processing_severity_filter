3e25101ebf26923a19469fc3f4b00e78de732e38
Aspect13 <ad13box@gmail.com>
2021-12-02 00:52:05 +0300

    integration fixes

--- components.py
+++ components.py
@@ -1,21 +1,7 @@
 from flask import render_template
 
 
-def render_integration_create_modal(context, slot, payload):
-    return render_template(
-        'severity_filter_integration.html',
-        config=payload
-    )
-
-
-def render_integration_card(context, slot, payload):
-    return render_template(
-        'severity_filter_integration_card.html',
-        config=payload
-    )
-
-
-def render_reporter_toggle(context, slot, payload):
+def render_toggle(context, slot, payload):
     integrations = context.rpc_manager.call.integrations_get_project_integrations_by_name(
         payload['id'],
         'processing_severity_filter'
