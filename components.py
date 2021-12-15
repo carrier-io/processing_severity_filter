663fee9be2e476f5d3d68b52f180aa8a46084cee
Aspect13 <ad13box@gmail.com>
2021-12-15 19:45:34 +0300

    fixing integration to be all alike

--- components.py
+++ components.py
@@ -1,13 +1,13 @@
 from flask import render_template
 
 
-def render_toggle(context, slot, payload):
+def render_test_toggle(context, slot, payload):
     integrations = context.rpc_manager.call.integrations_get_project_integrations_by_name(
         payload['id'],
         'processing_severity_filter'
     )
     payload['project_integrations'] = integrations
     return render_template(
-        'processing_severity_filter:severity_filter_toggle.html',
+        'processing_severity_filter:processing_severity_filter_test_toggle.html',
         config=payload
     )
