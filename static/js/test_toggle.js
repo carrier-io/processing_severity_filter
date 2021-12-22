8484b8a5c51d340725b426e61fe3c88cf851c90e
Aspect13 <ad13box@gmail.com>
2021-12-22 23:21:32 +0300

    refactored security refactored api and test run

--- static/js/test_toggle.js
+++ static/js/test_toggle.js
@@ -1,15 +1,13 @@
 window['processing_processing_severity_filter'] = {
-
     get_data: () => {
-        if ($('#integration_checkbox_reporter_jira').prop('checked')) {
+        if ($('#integration_checkbox_processing_severity_filter').prop('checked')) {
             return $('#processing_severity_processing_severity_filter .selectpicker').val()
         }
-
     },
     set_data: data => {
         $('#processing_severity_filter .selectpicker').val(data || 'Info').selectpicker('refresh')
     },
     clear_data: () => {
-        $('processing_severity_filter .selectpicker').val('Info').selectpicker('refresh')
+        $('#processing_severity_filter .selectpicker').val('Info').selectpicker('refresh')
     }
 }
