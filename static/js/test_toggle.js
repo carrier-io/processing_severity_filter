86cf01cfb925e515b2dd270b3f4e1f2559d51d14
Aspect13 <ad13box@gmail.com>
2022-01-10 20:18:52 +0300

    finalizing core validation functionality

--- static/js/test_toggle.js
+++ static/js/test_toggle.js
@@ -6,6 +6,8 @@ window['processing_processing_severity_filter'] = {
     },
     set_data: data => {
         $('#processing_severity_filter .selectpicker').val(data || 'Info').selectpicker('refresh')
+        $('#integration_checkbox_processing_severity_filter').prop('checked', true)
+        $('#selector_processing_severity_filter').collapse('show')
     },
     clear_data: () => {
         $('#processing_severity_filter .selectpicker').val('Info').selectpicker('refresh')
